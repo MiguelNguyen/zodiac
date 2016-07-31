24113bcb4663181588d714591bf9cde3c2e8d53a
Michael Nguyen <michaelnguyen@michaels-air.attlocal.net>
2016-07-30 20:41:20 -0700

    Updated and working

--- js/main.js
+++ js/main.js
@@ -1,76 +1,76 @@
-var horoscopesAndDescriptions = [
+var horoscopes = [
 {
-	horoscope: "Cancer",
+	sign: "cancer",
 	description: "Your best attributes are being awesome! always having a blast! and always making sure your loved ones are taken care of.",
-	image: "../img/cancer.png"
+	image: "img/cancer.png"
 },
 {
-	horoscope: "Leo",
+	sign: "leo",
 	description: "Your best attributes include ruling the jungle, generous tendencies, loyalty, love and care, initiative.",
-	image: "../img/leo.png"
+	image: "img/leo.png"
 },
 {
-	horoscope: "Capricorn",
+	sign: "capricorn",
 	description: "Your best attributes are: Determination, Dominance, Perservering, Practical, Willful",
-	image: "../img/Capricorn.png"
+	image: "img/Capricorn.png"
 },
 {
-	horoscope: "Gemini",
+	sign: "gemini",
 	description: "You're best attributes are: Communication, Indecision, Inquisitive, Intelligent, Changeable",
-	image: "../img/gemini.png"
+	image: "img/gemini.png"
 },
 {
-	horoscope: "Sagittarius",
+	sign: "sagittarius",
 	description: "Your best attributes are: Philosophical, Motion, Experimentation, Optimism",
-	image: "../img/sagittarius.png"
+	image: "img/sagittarius.png"
 },
 {
-	horoscope: "Aries",
+	sign: "aries",
 	description: "Your best attributes are: Active, Demanding, Determined, Effective, Ambitious",
-	image: "../img/aries.png"
+	image: "img/aries.png"
 },
 {
-	horoscope: "Taurus",
+	sign: "taurus",
 	description: "Your best attributes are: Security, Subtle strength, Appreciation, Instruction, Patience",
-	image: "../img/taurus.png"
+	image: "img/taurus.png"
 },
 {
-	horoscope: "Aquarius",
+	sign: "aquarius",
 	description: "Your best attributes are: Knowledge, Humanitarian, Serious, Insightful, Duplicitous",
-	image: "../img/aquarius.png"
+	image: "img/aquarius.png"
 },
 {
-	horoscope: "Libra",
+	sign: "libra",
 	description: "Your best attributes are: Balance, Justice, Truth, Beauty, Perfection",
-	image: "../img/libra.png"
+	image: "img/libra.png"
 },
 {
-	horoscope: "Scorpio",
+	sign: "scorpio",
 	description: "You're best attributes are: Transient, Self-Willed, Purposeful, Unyielding",
-	image: "../img/scorpio.png"
+	image: "img/scorpio.png"
 },
 {
-	horoscope: "Virgo",
+	sign: "virgo",
 	description: "You're best attributes are: Analyzing, Practical, Reflective, Observation, Thoughtful",
-	image: "../img/virgo.png"
+	image: "img/virgo.png"
 },
 {
-	horoscope: "Pisces",
+	sign: "pisces",
 	description: "You're best attributes are: Fluctuation, Depth, Imagination, Reactive, Indecisive",
-	image: "../img/pisces.png"
+	image: "img/pisces.png"
 
 }
 ]
 
-function horoscope() {
-	var horoscope = document.getElementById("horoscope").value.toLowerCase()
+function getZodiac() {
+  var user = document.getElementById("info").value.toLowerCase()
 	
 
-	for(i = 0; i < horoscopesAndDescriptions.length; i++){
-		if(horoscope === horoscopesAndDescriptions[i].horoscope){
-			document.getElementById("yourZodiac").innerHTML = horoscopesAndDescriptions[i].horoscope
-			document.getElementById("zodiacImg").src = horoscopesAndDescriptions[i].image
-			document.getElementById("yourHoroscope").innerHTML = horoscopesAndDescriptions.description
+	for(i = 0; i < horoscopes.length; i++){
+		if(user === horoscopes[i].sign){
+			document.getElementById("yourZodiac").innerHTML = horoscopes[i].sign
+			document.getElementById("zodiacImg").src = horoscopes[i].image
+			document.getElementById("yourHoroscope").innerHTML = horoscopes[i].description
 		return
 		} else {
 			document.getElementById("yourZodiac").innerHTML = "Please enter a Zodiac animal!!"
